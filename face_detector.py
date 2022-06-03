5922019465d2c791a2fc2df6d30005bc75732674
Adele Chinda <chindahadele2@gmail.com>
2022-06-04 00:09:18 +0300

    Greyscaling

--- face_detector.py
+++ face_detector.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
 import cv2
 
-trained_faces_data = cv2.CascadeClassifier("/home/logan/github/Face_Detector/haarcascade_frontalface_default.xml")
+trained_faces_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
+# Choose image to detect
+img = cv2.inread("RDJ.jpg")
+
+# Convert the data to greyscale
+
+
+print("code completed")
