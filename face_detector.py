5310d091720fc5f1a32fc51e4d78406f1301d89a
Adele Chinda <chindahadele2@gmail.com>
2022-06-04 01:10:15 +0300

    CMake re-run

--- face_detector.py
+++ face_detector.py
@@ -4,9 +4,11 @@ import cv2
 trained_faces_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # Choose image to detect
-img = cv2.inread("RDJ.jpg")
+img = cv2.imread("RDJ.jpg")
 
+cv2.imshow("Test data", img)
+cv2.waitKey()
 # Convert the data to greyscale
 
 
-print("code completed")
+#print("code completed")
