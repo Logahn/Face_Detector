6e74d4d281ee40f3bcbbcf037dc11a0538b140e1
Adele Chinda <chindahadele2@gmail.com>
2022-06-04 01:30:08 +0300

    Greyscale test data

--- face_detector.py
+++ face_detector.py
@@ -6,9 +6,13 @@ trained_faces_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml"
 # Choose image to detect
 img = cv2.imread("RDJ.jpg")
 
-cv2.imshow("Test data", img)
-cv2.waitKey()
 # Convert the data to greyscale
+greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+
+
+cv2.imshow("Test data", img)
+cv2.imshow("Test data", greyscaled_img)
+cv2.waitKey()
 
 #print("code completed")
