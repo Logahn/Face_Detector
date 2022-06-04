864c3f7711f9e6e8ec603d352a7b3f4b4b94659c
Adele Chinda <chindahadele2@gmail.com>
2022-06-04 04:02:27 +0300

    random.choice(color)
    Created random primary colors for the detector rectangle

--- face_detector.py
+++ face_detector.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import cv2
-import  random
+import  random 
 
 trained_faces_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # Choose image to detect
-img = cv2.imread("RDJ2.jpg")
+img = cv2.imread("images/multiple.png")
 
 # Convert the data to greyscale
 greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -16,15 +16,10 @@ face_coordinates = trained_faces_data.detectMultiScale(greyscaled_img)
 # [[63 39 78 78]]
 
 # Draw rectangles around faces
-# cv2.rectangle(img, (63, 39), (63+78, 39+78), (0, 255,0), 2)
-
-b = random.randint(0,255)
-g = random.randint(0,255)
-r = random.randint(0,255)
-
+colors = ((256,0,0), (0,256,0), (0,0,256))
 
 for (x, y, w, h) in face_coordinates:
-    color = (r,b,g)
+    color = random.choice(colors)
     cv2.rectangle(img, (x, y),(x+w, y+h), color, 2)
 
 
